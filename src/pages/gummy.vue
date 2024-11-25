dbdc1b73e6c806e4ecc0769e2dfa7e8e9106b32b
stanatadyen <stan.zakratskiy@adyen.com>
2024-11-25 17:25:06 +0100

    Update gummy.vue

--- src/pages/gummy.vue
+++ src/pages/gummy.vue
@@ -1,6 +1,5 @@
 <template>
     <div>
-        <h2>I am the gummy page inside the micro frontend</h2>
-        <span>Now I have some more stuff</span>
+        <h2>Stan was here</h2>
     </div>
 </template>
